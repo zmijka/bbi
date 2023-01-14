4968d1ee2777c9b0a0a27685d5274102049be084
Krzysztof Zmijewski <>
2023-01-14 23:39:09 +0100

    Init

---
+++ bbi.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# a tor exit IP
+ipset -N tor-exit-ip iphash
+
+wget -q https://check.torproject.org/cgi-bin/TorBulkExitList.py -O -|sed '/^#/d' | \
+
+while read IP_tor
+do
+  ipset -q -A tor-exit-ip $IP_tor
+done
+
+
+# a ISC bad IP list
+
+ipset -N isc iphash
+
+wget -q https://isc.sans.edu/ipsascii.html?limit=1000 -O -|sed -e '/^#/d' |awk '{ print $1 }'| sed -e 's/^[0]*//' -e 's/\.[0]*/\./g' | sed -e 's/\.\./\.0\./g' | \
+
+while read IP_isc
+do
+  ipset -q -A isc $IP_isc
+done
+
+
+# drop IP addr in iptables
+
+iptables -I INPUT -m set --match-set tor-exit-ip src -j DROP
+iptables -I INPUT -m set --match-set isc src -j DROP
