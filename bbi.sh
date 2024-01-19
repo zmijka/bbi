f24f0f6988d5d32f04d7ade47ae58d625272fdf1
Krzysztof Zmijewski <>
2024-01-19 23:34:13 +0100

    Fixed many bugs

--- bbi.sh
+++ bbi.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+CHAIN_tor=`iptables -S | grep tor | wc -l`
+CHAIN_isc=`iptables -S | grep isc | wc -l`
+
 # a tor exit IP
 ipset -N tor-exit-ip iphash
 
@@ -7,7 +10,7 @@ wget -q https://check.torproject.org/cgi-bin/TorBulkExitList.py -O -|sed '/^#/d'
 
 while read IP_tor
 do
-  ipset -q -A tor-exit-ip $IP_tor
+    ipset -q -A tor-exit-ip $IP_tor
 done
 
 
@@ -19,11 +22,19 @@ wget -q https://isc.sans.edu/ipsascii.html?limit=1000 -O -|sed -e '/^#/d' |awk '
 
 while read IP_isc
 do
-  ipset -q -A isc $IP_isc
+    ipset -q -A isc $IP_isc
 done
 
 
 # drop IP addr in iptables
 
-iptables -I INPUT -m set --match-set tor-exit-ip src -j DROP
-iptables -I INPUT -m set --match-set isc src -j DROP
+if [ $CHAIN_tor -eq "0" ]
+then
+    iptables -I INPUT -m set --match-set tor-exit-ip src -j DROP
+fi
+
+
+if [ $CHAIN_isc -eq "0" ]
+then
+    iptables -I INPUT -m set --match-set isc src -j DROP
+fi
